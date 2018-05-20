e54207f611ce5cb037ee1ae48731da5773f8db0b
bobcanthelpyou <bob@canthelpyou.com>
2018-05-20 14:47:39 +0200

    FFIL: release v1.4.7

--- site.mk
+++ site.mk
@@ -25,7 +25,7 @@ GLUON_SITE_PACKAGES := \
 	iptables \
 	haveged
 
-DEFAULT_GLUON_RELEASE := 1.4.6-ffil~$(shell date '+%Y%m%d')
+DEFAULT_GLUON_RELEASE := 1.4.7-ffil~$(shell date '+%Y%m%d')
 
 # Allow overriding the release number from the command line
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
