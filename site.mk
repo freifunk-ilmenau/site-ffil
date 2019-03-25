1d7e40a0e4b02a582b2cc5eff833bf3d2a50ab3d
bobcanthelpyou <bob@canthelpyou.com>
2019-03-25 01:53:29 +0100

    Add description from Gluon docs

--- site.mk
+++ site.mk
@@ -1,3 +1,8 @@
+##	GLUON_FEATURES
+#		Specify Gluon features/packages to enable;
+#		Gluon will automatically enable a set of packages
+#		depending on the combination of features listed
+
 GLUON_FEATURES := \
 	autoupdater \
 	ebtables-filter-multicast \
@@ -11,22 +16,41 @@ GLUON_FEATURES := \
 	web-advanced \
 	web-wizard
 
+##	GLUON_SITE_PACKAGES
+#		Specify additional Gluon/OpenWrt packages to include here;
+#		A minus sign may be prepended to remove a packages from the
+#		selection that would be enabled by default or due to the
+#		chosen feature flags
+
 GLUON_SITE_PACKAGES := \
 	iptables \
 	iwinfo \
 	haveged
 
+##	DEFAULT_GLUON_RELEASE
+#		version string to use for images
+#		gluon relies on
+#			opkg compare-versions "$1" '>>' "$2"
+#		to decide if a version is newer or not.
+
 DEFAULT_GLUON_RELEASE := 2018.1.4.0-ffil~$(shell date '+%Y%m%d')
 
-# Allow overriding the release number from the command line
+# Variables set with ?= can be overwritten from the command line
+
+##	GLUON_RELEASE
+#		call make with custom GLUON_RELEASE flag, to use your own release version scheme.
+#		e.g.:
+#			$ make images GLUON_RELEASE=23.42+5
+#		would generate images named like this:
+#			gluon-ff%site_code%-23.42+5-%router_model%.bin
+
 GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
 
+# Default priority for updates.
 GLUON_PRIORITY ?= 15
 
-GLUON_LANGS ?= de en
-
-# Build images to update from EU-specific firmware (TP-Link)
+# Region code required for some images; supported values: us eu
 GLUON_REGION := eu
 
-# Build images with ath10k-based drivers for IBSS (Ad-Hoc)
-GLUON_ATH10K_MESH := 11s
+# Languages to include
+GLUON_LANGS ?= de en
